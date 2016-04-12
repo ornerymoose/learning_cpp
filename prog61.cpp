7b0a545486b36a39ea62049e65736416a536059c
ornerymoose <dpardy87@gmail.com>
2016-04-12 14:49:00 -0400

    calling a function from main(), simple example

---
+++ prog61.cpp
@@ -0,0 +1,13 @@
+//calling a function
+#include <iostream>
+using namespace std;
+
+void greeting(){
+	cout << "Hi there!" << endl;
+}
+
+int main() {
+	cout << "howdy from the main() function!" << endl;
+	greeting();
+	return 0;
+}
